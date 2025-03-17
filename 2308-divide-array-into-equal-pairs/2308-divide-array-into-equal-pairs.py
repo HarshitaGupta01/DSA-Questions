47386ab155360f577b7eddcef9e31d78b2395408
HarshitaGupta01 <harshitagupta093@gmail.com>
2025-03-17 16:09:03 +0530

    Time: 4 ms (72.87%), Space: 12.5 MB (65.96%) - LeetHub

---
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def divideArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        d={}
+        for i in nums:
+            if i in d:
+                d[i]+=1
+            else:
+                d[i]=1
+        for j in d.values():
+            if j%2!=0:
+                return False
+        return True
+        
