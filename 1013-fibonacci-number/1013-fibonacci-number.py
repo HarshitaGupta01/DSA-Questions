78795bbc3e7f75526670b347c91eefbb50b2daa0
HarshitaGupta01 <harshitagupta093@gmail.com>
2025-04-08 10:18:27 +0530

    Time: 11 ms (92.04%), Space: 12.5 MB (10.61%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -4,7 +4,17 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        # ====recursion=========
+        # if n<=1:
+        #     return n
+        # else:
+        #     return self.fib(n-1)+ self.fib(n-2)
+
+        # =========
+        dp=[0]*(n+1)
         if n<=1:
             return n
-        else:
-            return self.fib(n-1)+ self.fib(n-2)
+        dp[1]=1
+        for i in range(2,n+1):
+            dp[i]=dp[i-1]+dp[i-2]
+        return dp[n]
