8dc4aa171d2fb160c796e8b6efa405beb76616df
Harshita Gupta <harshitagupta093@gmail.com>
2025-04-24 12:26:57 +0530

    Time: 9 ms (26.67%), Space: 12.7 MB (10.62%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        # ==recursion===================dfs=============
+        # def rec(idx):
+        #     if idx==len(s):
+        #         return True
+        #     for i in range(idx+1,len(s)+1):
+        #         if s[idx:i] in wordDict and rec(i):
+        #             return True
+        #     return False
+        # return rec(0)
+
+        # =======memoization==========
+        memo={}
+        def rec(idx):
+            if idx==len(s):
+                return True
+            if idx in memo:
+                return memo[idx]
+            for i in range(idx+1,len(s)+1):
+                if s[idx:i] in wordDict and rec(i):
+                    memo[idx]=True
+                    return True
+            memo[idx]=False
+            return False
+        return rec(0)
+
+
+
+        
