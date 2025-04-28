188772eacc20ff7b70f5ba86d06f2bb65847e740
Harshita Gupta <harshitagupta093@gmail.com>
2025-04-28 14:52:23 +0530

    Time: 514 ms (84.98%), Space: 33.7 MB (53.85%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -16,18 +16,30 @@ class Solution(object):
         # return rec(0,0)
 
         # ========memoization===========
-        memo={}
-        def rec(i,j):
-            if i==len(text1) or j==len(text2):
-                return 0
-            if (i,j) in memo:
-                return memo[(i,j)]
-            if text1[i]==text2[j]:
-                return 1+rec(i+1,j+1)
-            else:
-                memo[(i,j)]= max(rec(i+1,j), rec(i,j+1))
-                return memo[(i,j)]
-        return rec(0,0)
+        # memo={}
+        # def rec(i,j):
+        #     if i==len(text1) or j==len(text2):
+        #         return 0
+        #     if (i,j) in memo:
+        #         return memo[(i,j)]
+        #     if text1[i]==text2[j]:
+        #         return 1+rec(i+1,j+1)
+        #     else:
+        #         memo[(i,j)]= max(rec(i+1,j), rec(i,j+1))
+        #         return memo[(i,j)]
+        # return rec(0,0)
+
+        # ===============tabulation====
+        m=len(text1)
+        n=len(text2)
+        dp=[[0]*(n+1) for _ in range (m+1)]
+        for i in range(0,m):
+            for j in range(0,n):
+                if text1[i]==text2[j]:
+                    dp[i+1][j+1]=1+dp[i][j]
+                else:
+                    dp[i+1][j+1]=max(dp[i][j+1],dp[i+1][j])
+        return dp[m][n]
 
 
         
