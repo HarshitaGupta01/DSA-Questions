499c539d8362dd7d7534916aee4752adb717c281
Harshita Gupta <harshitagupta093@gmail.com>
2025-04-28 14:45:36 +0530

    Time: 4192 ms (6.74%), Space: 197.6 MB (6.96%) - LeetHub

---
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def longestCommonSubsequence(self, text1, text2):
+        """
+        :type text1: str
+        :type text2: str
+        :rtype: int
+        """
+        # # ==recursion=====================
+        # def rec(i,j):
+        #     if i==len(text1) or j==len(text2):
+        #         return 0
+        #     if text1[i]==text2[j]:
+        #         return 1+rec(i+1,j+1)
+        #     else:
+        #         return max(rec(i+1,j), rec(i,j+1))
+        # return rec(0,0)
+
+        # ========memoization===========
+        memo={}
+        def rec(i,j):
+            if i==len(text1) or j==len(text2):
+                return 0
+            if (i,j) in memo:
+                return memo[(i,j)]
+            if text1[i]==text2[j]:
+                return 1+rec(i+1,j+1)
+            else:
+                memo[(i,j)]= max(rec(i+1,j), rec(i,j+1))
+                return memo[(i,j)]
+        return rec(0,0)
+
+
+        
